8363cbe5cf90b50bd2da87e05085ad09c5ea1116
xiaoming <xiaoming@qq.com>
2022-03-01 21:33:48 +0800

    第4次提交， 修改User.java  hot-fix 提交

--- src/main/java/cn/itcast/git/User.java
+++ src/main/java/cn/itcast/git/User.java
@@ -10,5 +10,29 @@ public class User {
 
     private String name;
 
-     private String email;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
